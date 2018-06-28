fcca86a6131d212d3b8b80b37331289ccb71732f
Mark Lee <mark@lob.com>
2018-06-28 15:47:34 -0700

    feat(areas-routes): remove resources

--- lib/lob/area.ex
+++
@@ -1,8 +0,0 @@
-defmodule Lob.Area do
-  @moduledoc """
-  Module implementing the Lob areas API.
-  """
-
-  use Lob.ResourceBase, endpoint: "areas", methods: [:create, :retrieve, :list]
-
-end
--- lib/lob/route.ex
+++
@@ -1,8 +0,0 @@
-defmodule Lob.Route do
-  @moduledoc """
-  Module implementing the Lob routes API.
-  """
-
-  use Lob.ResourceBase, endpoint: "routes", methods: [:retrieve, :list]
-
-end
